a7201706353a0f2cf08b7f4ddb118e327a81d62f
RaymovMardon <googlaccnew12@gmail.com>
2025-08-11 22:34:10 +0500

    commet

--- src/components/layout/Index.jsx
+++ src/components/layout/Index.jsx
@@ -1,17 +1,29 @@
-import { React, useState } from "react";
+import { React, useEffect, useState } from "react";
 import Header from "../header/Index";
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigate } from "react-router-dom";
 import Sitebar from "../sitebar/Index";
 
 const Adminlayout = () => {
-  const [collapsed, setCollapsed] = useState(false)
-
+  const [collapsed, setCollapsed] = useState(false);
+  const navigate = useNavigate();
+   const token = localStorage.getItem("token");
+ useEffect(()=>{
+   if(!token){
+     navigate("/login")
+   }else{
+    navigate("/statistics")
+   }
+ },[token])
   return (
     <>
-      <Header collapsed={collapsed} setCollapsed={setCollapsed}/>
+      <Header collapsed={collapsed} setCollapsed={setCollapsed} />
       <Sitebar collapsed={collapsed} />
       <div>
-        <main className={`transition-all duration-300 ${collapsed ? 'ml-[90px]' : 'ml-[258px] mr-[6px]'} mt-[10px]`}>
+        <main
+          className={`transition-all duration-300 ${
+            collapsed ? "ml-[90px]" : "ml-[258px] mr-[6px]"
+          } mt-[10px]`}
+        >
           <Outlet />
         </main>
       </div>
