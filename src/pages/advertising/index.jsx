f22fe4e2173eb32c7b0a6fe6e2664be6db3f0dab
RaymovMardon <googlaccnew12@gmail.com>
2025-08-19 23:58:30 +0500

    update back ChartGraph for canvas edit adverising  19/08/25 23:58

--- src/pages/advertising/index.jsx
+++ src/pages/advertising/index.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 
 function AdvertisingPage() {
-  const [statistic, setStatistic] = useState([]);
+  const [advertising, setAdvertising] = useState([]);
   const [region, setRegion] = useState([]);
   const [boshlangich, setBoshlangich] = useState([]);
   const [yakuniy, setYakuniy] = useState([]);
@@ -11,13 +11,8 @@ function AdvertisingPage() {
     try {
       const res = await API.get("/advertisement");
       console.log(res.data.data);
-      setStatistic(res.data.data);
-      setRegion(res.data.data.map((item) => item.regionName));
-      setBoshlangich(
-        res.data.data.map((item) => item.averageInitialPercentage)
-      );
-      setYakuniy(res.data.data.map((item) => item.averageFinalPercentage));
-      setNatija(res.data.data.map((item) => item.growthPercentage));
+      setAdvertising(res.data.data);
+      console.log(advertising);
     } catch (err) {
       console.error(err);
     }
